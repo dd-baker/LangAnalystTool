3cafb6decbc210aca5c07deb45ab328b10145a81
Devin Baker <constructingintelligence@outlook.com>
2020-03-26 18:23:54 -0500

    first commit

---
+++ src/main/java/com/natomicanalysis/LangAnalystTool/LangAnalystToolApplication.java
@@ -0,0 +1,13 @@
+package com.natomicanalysis.LangAnalystTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangAnalystToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LangAnalystToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/natomicanalysis/LangAnalystTool/LangAnalystToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natomicanalysis.LangAnalystTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangAnalystToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
